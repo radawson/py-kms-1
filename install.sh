841c9e7d08afa5be22e2ca55d0339bc3dbffa44d
Rick Dawson <rdbh@users.noreply.github.com>
2020-12-14 21:01:18 -0500

    Create install.sh
    added install script for use on openwrt

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Installs py-kms as a service on OpenWRT devices
+# Run as root
+
+cp ./resources/kms-server /etc/init.d/kms-server
+chmod 755 /etc/init.d/kms-server
+/etc/init.d/kms-server enable
+
+echo "py-kms installed as a process that starts on boot"
+echo "py-kms will autodetect the LAN bridge IP address on reboot or restart"
+echo -e "If you change the IP in the GUI, you must still manually restart\nthe kms-server."
+echo -e "\nTo start the server manually, type '/etc/init.d/kms-server start'"
+echo -e "\nTo stop the server, type '/etc/init.d/kms-server stop'"
+echo -e "\nTo restart the server, type '/etc/init.d/kms-server restart'"
+echo -e "\n\nProblems? https://github.com/radawson/py-kms/issues"
+echo -e "\n\nStarting KMS server now"
+
+/etc/init.d/kms-server start
