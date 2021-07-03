d2a3f595aededb2744f1c21a4c7713f2c97b1dcb
Ubuntu <ubuntu@nextcloudwa.wa.partridgexing.org>
2021-07-03 01:13:29 +0000

    separated openwrt and ubuntu options

--- install.sh
+++ install.sh
@@ -2,17 +2,33 @@
 # Installs py-kms as a service on OpenWRT devices
 # Run as root
 
-sudo cp ./resources/kms-server /etc/init.d/kms-server
-sudo chmod 755 /etc/init.d/kms-server
-sudo /etc/init.d/kms-server enable
+setup_ubuntu(){
+sudo cp ./resources/kms-server-ubuntu /lib/systemd/system/kms-server.service
+sudo systemctl daemon-reload
+sudo systemctl enable test-py.service
+printf "$message"
+sudo systemctl start test-py.service
+}
 
-echo "py-kms installed as a process that starts on boot"
-echo "py-kms will autodetect the LAN bridge IP address on reboot or restart"
-echo -e "If you change the IP in the GUI, you must still manually restart\nthe kms-server."
-echo -e "\nTo start the server manually, type '/etc/init.d/kms-server start'"
-echo -e "\nTo stop the server, type '/etc/init.d/kms-server stop'"
-echo -e "\nTo restart the server, type '/etc/init.d/kms-server restart'"
-echo -e "\n\nProblems? https://github.com/radawson/py-kms/issues"
-echo -e "\n\nStarting KMS server now"
+setup_openwrt(){
+cp ./resources/kms-server-owrt /etc/init.d/kms-server
+chmod 755 /etc/init.d/kms-server
+/etc/init.d/kms-server enable
+message+="py-kms will autodetect the LAN bridge IP address on reboot or restart"
+message+="If you change the IP in the GUI, you must still manually restart\nthe>
+message+="\nTo start the server manually, type '/etc/init.d/kms-server start'"
+message+="\nTo stop the server, type '/etc/init.d/kms-server stop'"
+printf "$message"
+/etc/init.d/kms-server start
+}
 
-sudo /etc/init.d/kms-server start
+# TODO: modify the message for each OS use case
+message="\npy-kms installed as a process that starts on boot\n"
+message+="\n\nProblems? https://github.com/radawson/py-kms/issues"
+message+="\n\nStarting KMS server now\n"
+
+# TODO: detect operating system and run appropriate script
+
+setup_openwrt
+printf "\n\nProblems? https://github.com/radawson/py-kms/issues"
+printf "\n\nStarting KMS server now\n"
