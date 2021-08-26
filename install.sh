1c664885b093b68ae48f5e65aa8e084d09e5177d
Rick Dawson <dawsonra@gmail.com>
2021-08-26 17:30:37 -0400

    another try at fixing hte message error

--- install.sh
+++ install.sh
@@ -4,23 +4,24 @@
 message=""
 
 setup_ubuntu(){
-sudo cp ./resources/kms-server-ubuntu /lib/systemd/system/kms-server.service
-sudo systemctl daemon-reload
-sudo systemctl enable kms-server.service
-printf "$message"
-sudo systemctl start kms-server.service
+	sudo cp ./resources/kms-server-ubuntu /lib/systemd/system/kms-server.service
+	sudo systemctl daemon-reload
+	sudo systemctl enable kms-server.service
+	printf "$message"
+	sudo systemctl start kms-server.service
 }
 
 setup_openwrt(){
-cp ./resources/kms-server-owrt /etc/init.d/kms-server
-chmod 755 /etc/init.d/kms-server
-/etc/init.d/kms-server enable
-message+="py-kms will autodetect the LAN bridge IP address on reboot or restart"
-message+="If you change the IP in the GUI, you must still manually restart\n"
-message+="\nTo start the server manually, type '/etc/init.d/kms-server start'"
-message+="\nTo stop the server, type '/etc/init.d/kms-server stop'"
-printf "$message"
-/etc/init.d/kms-server start
+	cp ./resources/kms-server-owrt /etc/init.d/kms-server
+	chmod 755 /etc/init.d/kms-server
+	/etc/init.d/kms-server enable
+
+	message+="py-kms will autodetect the LAN bridge IP address on reboot or restart"
+	message+="If you change the IP in the GUI, you must still manually restart\n"
+	message+="\nTo start the server manually, type '/etc/init.d/kms-server start'"
+	message+="\nTo stop the server, type '/etc/init.d/kms-server stop'"
+	printf "$message"
+	/etc/init.d/kms-server start
 }
 
 # TODO: modify the message for each OS use case
